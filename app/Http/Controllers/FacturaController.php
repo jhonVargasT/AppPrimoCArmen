b89c727758a432c2f8c633dd1c81266670d2ba7f
ar2r91 <theoithy@gmail.com>
2018-12-16 09:54:35 -0500

    Version 30 AVANCE

--- app/Http/Controllers/FacturaController.php
+++ app/Http/Controllers/FacturaController.php
@@ -554,10 +554,10 @@ class FacturaController extends Controller
 
     private function consumo_soap($filename)
     {
-        //(URL PRODUCCION https://e-factura.sunat.gob.pe/ol-ti-itcpfegem/billService?wsdl)
-        //$wsdlURL = 'https://e-beta.sunat.gob.pe/ol-ti-itcpfegem-beta/billService?wsdl';
+        //$wsdlURL = 'https://e-factura.sunat.gob.pe/ol-ti-itcpfegem/billService?wsdl';//PRODUCCION
+        //$wsdlURL = 'https://e-beta.sunat.gob.pe/ol-ti-itcpfegem-beta/billService?wsdl';//BETA
         $nombre_archivo = $filename . '.zip';
-        $wsdlURL = 'https://e-beta.sunat.gob.pe/ol-ti-itcpfegem-beta/billService?wsdl';
+        $wsdlURL = 'https://e-factura.sunat.gob.pe/ol-ti-itcpfegem/billService?wsdl';
         $XMLString = '<?xml version="1.0" encoding="UTF-8"?>
         <soapenv:Envelope 
         xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
